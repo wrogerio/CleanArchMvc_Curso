a2701aa248dde99bad92f981f90de388761bc333
wrogerio <wrogerio.ing@gmail.com>
2021-08-25 01:12:17 -0300

    criação e configuração das entidades

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : EntityBase
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required!");
+            DomainExceptionValidation.When(name != null && name.Length < 3, "Invalid name. To short, minimum 3 characters");
+            Name = name;
+        }
+    }
+}
